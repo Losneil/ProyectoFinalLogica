965c4d877f1421a67151068d5c9ecf469f02a378
Jose Quintero <56616494+Losneil@users.noreply.github.com>
2022-05-27 14:40:49 -0500

    Nueva clase Empresa

---
+++ src/proyectofinal/Empresa.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectofinal;
+
+/**
+ *
+ * @author JOSE QUINTERO
+ */
+public class Empresa {
+    Usuario usuarios[] = new Usuario[2];
+    Complejo complejos[];
+    Pelicula peliculas[];
+}
