b6759a3556a0b5a7fcf4d5f90ff16f107f1d75bd
XDUrsK <92643313+XDUrsK@users.noreply.github.com>
2022-06-02 18:03:42 -0500

    Actu

--- src/proyectofinal/Programacion.java
+++ src/proyectofinal/Programacion.java
@@ -1,98 +1,100 @@
+
 package proyectofinal;
 
-import java.time.LocalTime;
 import javax.swing.JOptionPane;
-import java.util.ArrayList;
+
 /**
- * Fecha de creacion: 9 de mayo
- * @author Jose Manuel Quintero Rodriguez, Juan Angel Riaño
- * 
- * Esta clase, representa la programación que posee cualquier sala de un complejo,
- * o la programación de todo el complejo en general.
- * 
- */
+   * Fecha de creacion: 9 de mayo
+   *
+   * Esta clase, representa la programación que posee cualquier sala de un complejo, o la programación de todo el complejo en general.
+   * @author Jose Manuel Quintero Rodriguez.
+   * @author Juan Ángel Riaño Quintero.
+   *
+   */
 public class Programacion {
 
-    // Atributos de la clase
-    int sillasReservadas;
-    // Establecemos el formato de la hora para el horario
-    LocalTime horario = LocalTime.now();
-    String nombrePelicula;
-    int numeroSala;
-    boolean sillas[][] = new boolean[5][5];
-    
-    Pelicula pl = new Pelicula();
-    Complejo cp = new Complejo();
-    Sala sl = new Sala();
-    
-    /*Este método constructor se encarga de realizar asignación de valores a los atributos
-    @author Jose Manuel Quintero Rodriguez*/
-    public Programacion(int ns) {
-        this.numeroSala = ns;
-    }
-    
-    public void crearProgramacion(){
-        // Creamos una lista de arreglos que tomará los id de las salas
-//        ArrayList<Integer> listaSalas = new ArrayList<Integer>();
-//        try {
-//            listaSalas.add(cp.salas[0].programaciones[0].numeroSala);
-//            listaSalas.add(cp.salas[1].programaciones[1].numeroSala);
-//        } catch (NullPointerException e) {
-//            JOptionPane.showMessageDialog(null, "Hay un error de tipo " + e);
-//        }
-//        numeroSala = (JOptionPane.showInputDialog(null, "Elija el numero de la sala", "Salas",
-//                JOptionPane.PLAIN_MESSAGE, null, new Object[]{"Seleccionar", listaSalas}, "")).toString();
-        String listadoSalas = "";
-        for (int i = 0; i < cp.salas.length; i++) {
-            if(this.numeroSala == cp.salas[i].numeroSala){
-                listadoSalas += cp.salas[i].numeroSala;
-            } else {
-                JOptionPane.showMessageDialog(null, "Esa sala no existe");
-            }
-        }
-        nombrePelicula = JOptionPane.showInputDialog("Ingrese nombre de la pelicula");
-        // Generar el mapa de la sala sin asientos ocupados
-        for (int i = 0; i < sl.cantidadFilas; i++) {
-            for (int j = 0; j < sl.sillasPorFila; j++) {
-                sillas[i][j] = true;
-            }
-        }
-        String espacios = "";
-        for (int i = 0; i < sl.cantidadFilas; i++) {
-            for (int j = 0; j < sl.sillasPorFila; j++) {
-                espacios += sillas[i][j] + " ";
-            }
-            espacios += "\n";
-        }
-        JOptionPane.showMessageDialog(null, espacios);
-    }
-    
-    /*Método encargado de mostrar el mapa de una sala en un estado temporal durante la programacion de un filme*/
-    public void mostrarMapaProgramado(int idSala){
-        boolean existencia = false;
-        String muestreMapa = "";
-        for (int i = 0; i < cp.salas.length; i++) { // Recorremos el arreglo de la sala
-            if (cp.salas[i] != null) { // Verificamos si la sala existe
-                if (cp.salas[i].numeroSala == idSala) { // Verificamos si existe la sala
-                    existencia = true;
-                    for (int j = 0; j < cp.salas[i].cantidadFilas; j++) {
-                        for (int k = 0; k < cp.salas[i].sillasPorFila; k++) {
-                            try {
-                                muestreMapa += sl.programaciones[i].sillas[j][k] + " ";
-                            } catch(NullPointerException ex){
-                                JOptionPane.showMessageDialog(null, "No existen programaciones registradas");
-                            }
-                        }
-                        muestreMapa += "\n";
-                    }
-                }
-            } else {
-                JOptionPane.showMessageDialog(null, "No hay salas registradas en este complejo", "ERROR", JOptionPane.ERROR_MESSAGE);
-                break; // Rompemos el ciclo inmediatamente sacandonos del ciclo
-            }
-        } // En caso de que la existencia de esa sala especifica sea falsa
-        if (existencia == true) {
-            JOptionPane.showMessageDialog(null, muestreMapa);
-        }
-    }
+	 // Atributos de la clase.
+	 int sillasReservadas;
+	 String horario;
+	 int numeroSala;
+
+	 // La matriz de sillas[], siempre será una matriz 5x5, osea que todas las salas tendran el mismo tamaño.
+	 boolean sillas[][] = new boolean[5][5];
+
+//	 Pelicula pl = new Pelicula();
+//	 Complejo cp = new Complejo();
+//	 Sala sl = new Sala();
+
+	 /**
+		 * Este método constructor se encarga de realizar asignación de valores a los atributos.
+		 * @author Jose Manuel Quintero Rodriguez. 
+		 * 
+		 * @param numeroSala es el ID de la sala a la cual pertenece la programación. 
+		 * 
+		 */
+	 public Programacion(int numeroSala) {
+
+			this.numeroSala = numeroSala;
+			horario = "";		
+			sillasReservadas = 0;
+	  }
+
+
+
+
+//	 public void crearProgramacion() {
+
+//			String listadoSalas = "";
+//			for (int i = 0; i < cp.salas.length; i++) {
+//				 if (this.numeroSala == cp.salas[i].numeroSala) {
+//						listadoSalas += cp.salas[i].numeroSala;
+//				 } else {
+//						JOptionPane.showMessageDialog(null, "Esa sala no existe");
+//				 }
+//			}
+//			nombrePelicula = JOptionPane.showInputDialog("Ingrese nombre de la pelicula");
+//			// Generar el mapa de la sala sin asientos ocupados
+//			for (int i = 0; i < sl.cantidadFilas; i++) {
+//				 for (int j = 0; j < sl.sillasPorFila; j++) {
+//						sillas[i][j] = true;
+//				 }
+//			}
+//			String espacios = "";
+//			for (int i = 0; i < sl.cantidadFilas; i++) {
+//				 for (int j = 0; j < sl.sillasPorFila; j++) {
+//						espacios += sillas[i][j] + " ";
+//				 }
+//				 espacios += "\n";
+//			}
+//			JOptionPane.showMessageDialog(null, espacios);
+//	 }
+//
+//	 /*Método encargado de mostrar el mapa de una sala en un estado temporal durante la programacion de un filme*/
+//	 public void mostrarMapaProgramado(int idSala) {
+//			boolean existencia = false;
+//			String muestreMapa = "";
+//			for (int i = 0; i < cp.salas.length; i++) { // Recorremos el arreglo de la sala
+//				 if (cp.salas[i] != null) { // Verificamos si la sala existe
+//						if (cp.salas[i].numeroSala == idSala) { // Verificamos si existe la sala
+//							 existencia = true;
+//							 for (int j = 0; j < cp.salas[i].cantidadFilas; j++) {
+//									for (int k = 0; k < cp.salas[i].sillasPorFila; k++) {
+//										 try {
+//												muestreMapa += sl.programaciones[i].sillas[j][k] + " ";
+//										 } catch (NullPointerException ex) {
+//												JOptionPane.showMessageDialog(null, "No existen programaciones registradas");
+//										 }
+//									}
+//									muestreMapa += "\n";
+//							 }
+//						}
+//				 } else {
+//						JOptionPane.showMessageDialog(null, "No hay salas registradas en este complejo", "ERROR", JOptionPane.ERROR_MESSAGE);
+//						break; // Rompemos el ciclo inmediatamente sacandonos del ciclo
+//				 }
+//			} // En caso de que la existencia de esa sala especifica sea falsa
+//			if (existencia == true) {
+//				 JOptionPane.showMessageDialog(null, muestreMapa);
+//			}
+//	 }
 }
--- src/proyectofinal/Reserva.java
+++ src/proyectofinal/Reserva.java
@@ -1,59 +1,59 @@
-
+	 
 package proyectofinal;
 
-import javax.swing.JOptionPane;
-// Estos formatos sirven para determinar la hora y la fecha actuales
-import java.time.LocalDate;
-import java.time.LocalTime;
+	 import javax.swing.JOptionPane;
+	 // Estos formatos sirven para determinar la hora y la fecha actuales
+	 import java.time.LocalDate;
+	 import java.time.LocalTime;
 
-/**
- * Fecha de creación: 3 de mayo
- *
- * @author Jose Manuel Quintero Rodriguez Esta clase se encargará de pedir los datos para una reserva y generar una boleta por reserva realizada
- */
-public class Reserva {
+	 /**
+		* Fecha de creación: 3 de mayo
+		*
+		* @author Jose Manuel Quintero Rodriguez Esta clase se encargará de pedir los datos para una reserva y generar una boleta por reserva realizada
+		*/
+	 public class Reserva {
 
-	/* Atributos de la clase
-     @Author Jose Manuel Quintero Rodriguez*/
-	String nombreComplejo, nombrePelicula;
-	double precio;
-	int calificacion, numeroSala;
-	LocalDate fechaVenta = LocalDate.now();
-	LocalTime horaFuncion = LocalTime.now();
+			/* Atributos de la clase
+				@Author Jose Manuel Quintero Rodriguez*/
+			String nombreComplejo, nombrePelicula;
+			double precio;
+			int calificacion, numeroSala;
+			LocalDate fechaVenta = LocalDate.now();
+			LocalTime horaFuncion = LocalTime.now();
 
-	/* Método constructor que lo único que hace es inicializar los valores de los atrubutos
-    @author José Manuel Quintero Rodriguez*/
-	public Reserva() {
-		nombreComplejo = "";
-		nombrePelicula = "";
-		precio = 0;
-		calificacion = 0;
-		numeroSala = 0;
-	}
-
-	/* Este método permite ingresar la informacion de la boleta
-     @author Jose Manuel Quintero Rodriguez*/
-	public void pedirDatosBoleta() {
-		nombreComplejo = JOptionPane.showInputDialog("Ingrese el nombre del complejo");
-		nombrePelicula = JOptionPane.showInputDialog("Ingrese el nombre de la pelicula");
-		numeroSala = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número de la sala"));
-		precio = Double.parseDouble(JOptionPane.showInputDialog("Digite el precio de la boleta"));
-		do {
-			calificacion = Integer.parseInt(JOptionPane.showInputDialog("Como califica la pelicula de 1 a 10"));
-			if (calificacion < 0 || calificacion > 10) {
-				JOptionPane.showMessageDialog(null, "Calificacion no valida", "ERROR", JOptionPane.ERROR_MESSAGE);
+			/* Método constructor que lo único que hace es inicializar los valores de los atrubutos
+			 @author José Manuel Quintero Rodriguez*/
+			public Reserva() {
+				 nombreComplejo = "";
+				 nombrePelicula = "";
+				 precio = 0;
+				 calificacion = 0;
+				 numeroSala = 0;
 			}
-		} while (calificacion < 0 || calificacion > 10);
-	}
 
-	/*Esté método se encargará de generar la boleta al hacer una reserva
-    @author Jose Manuel Quintero Rodriguez*/
-	public void generarBoleta() {
-		JOptionPane.showMessageDialog(null, "Nombre del complejo: " + nombreComplejo
-			+ "\nNombre de la pelicula: " + nombrePelicula
-			+ "\nNúmero de la sala: " + numeroSala
-			+ "\nFecha de la venta: " + fechaVenta
-			+ "\nHora de la función: " + horaFuncion
-			+ "\nPrecio de la boleta: " + precio);
-	}
-}
+			/* Este método permite ingresar la informacion de la boleta
+				@author Jose Manuel Quintero Rodriguez*/
+			public void pedirDatosBoleta() {
+				 nombreComplejo = JOptionPane.showInputDialog("Ingrese el nombre del complejo");
+				 nombrePelicula = JOptionPane.showInputDialog("Ingrese el nombre de la pelicula");
+				 numeroSala = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número de la sala"));
+				 precio = Double.parseDouble(JOptionPane.showInputDialog("Digite el precio de la boleta"));
+				 do {
+						calificacion = Integer.parseInt(JOptionPane.showInputDialog("Como califica la pelicula de 1 a 10"));
+						if (calificacion < 0 || calificacion > 10) {
+							 JOptionPane.showMessageDialog(null, "Calificacion no valida", "ERROR", JOptionPane.ERROR_MESSAGE);
+						}
+				 } while (calificacion < 0 || calificacion > 10);
+			}
+
+			/*Esté método se encargará de generar la boleta al hacer una reserva
+			 @author Jose Manuel Quintero Rodriguez*/
+			public void generarBoleta() {
+				 JOptionPane.showMessageDialog(null, "Nombre del complejo: " + nombreComplejo
+				 + "\nNombre de la pelicula: " + nombrePelicula
+				 + "\nNúmero de la sala: " + numeroSala
+				 + "\nFecha de la venta: " + fechaVenta
+				 + "\nHora de la función: " + horaFuncion
+				 + "\nPrecio de la boleta: " + precio);
+			}
+	 }
