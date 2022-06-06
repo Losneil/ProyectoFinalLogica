45e45e7763427954318a943ef160ad602f20edef
Jose Quintero <56616494+Losneil@users.noreply.github.com>
2022-06-05 23:05:51 -0500

    Indentacion Comentarios y Controladores
    Ir actualizando así sean cambios pequeños.

--- src/proyectofinal/Complejo.java
+++ src/proyectofinal/Complejo.java
@@ -5,7 +5,12 @@ import javax.swing.JOptionPane;
 /**
  * Fecha de creacion: 3 de mayo
  *
- * Esta clase se encarga de pedir los datos de un complejo, ingresar un numero no mayor a 10 de salas mostrar el mapa de la sala mediante especificación de identificador, reservar una silla en una sala, calcular el valor recaudado por venta de boletas en un día por cada sala del complejo, y permite también registrar a los usuarios vendedor y administrador que pertenecerá al complejo.
+ * Esta clase se encarga de pedir los datos de un complejo, ingresar un numero
+ * no mayor a 10 de salas mostrar el mapa de la sala mediante especificación de
+ * identificador, reservar una silla en una sala, calcular el valor recaudado
+ * por venta de boletas en un día por cada sala del complejo, y permite también
+ * registrar a los usuarios vendedor y administrador que pertenecerá al
+ * complejo.
  *
  * @author Jose Manuel Quintero Rodríguez.
  * @author Juan Ángel Riaño Quintero.
@@ -13,85 +18,85 @@ import javax.swing.JOptionPane;
  */
 public class Complejo {
 
-	// Atributos de la clase.
-	String nombre, direccion, nombreAdmin;
+    // Atributos de la clase.
+    String nombre, direccion, nombreAdmin;
 
-	/* Crear arreglo de tipo sala que almacenará como máximo 10 salas por complejo
+    /* Crear arreglo de tipo sala que almacenará como máximo 10 salas por complejo
 		En este caso se dejó indicado el indice del numero de salas*/
-	Sala[] salas = new Sala[10];
-	Sala sl = new Sala();
+    Sala[] salas = new Sala[10];
+    Sala sl = new Sala();
 
-	/**
-	 * Método constructor que inicializa los valores de los atributos.
-	 *
-	 * @author José Manuel Quintero Rodriguez.
-	 *
-	 */
-	public Complejo() {
+    /**
+     * Método constructor que inicializa los valores de los atributos.
+     *
+     * @author José Manuel Quintero Rodriguez.
+     *
+     */
+    public Complejo() {
 
-		nombre = "";
-		direccion = "";
-		nombreAdmin = "";
-	}
+        nombre = "";
+        direccion = "";
+        nombreAdmin = "";
+    }
 
-	/**
-	 * Este método se encarga de pedir los datos del Complejo en general.
-	 *
-	 * @author José Manuel Quintero Rodríguez.
-	 *
-	 */
-	public void pedirInfoComplejo() {
-		nombre = JOptionPane.showInputDialog("Ingrese el nombre del complejo: ");
-		direccion = JOptionPane.showInputDialog("Ingrese la direccion del complejo: ");
-		nombreAdmin = JOptionPane.showInputDialog("Ingrese el nombre del administrador: ");
+    /**
+     * Este método se encarga de pedir los datos del Complejo en general.
+     *
+     * @author José Manuel Quintero Rodríguez.
+     *
+     */
+    public void pedirInfoComplejo() {
+        nombre = JOptionPane.showInputDialog("Ingrese el nombre del complejo: ");
+        direccion = JOptionPane.showInputDialog("Ingrese la direccion del complejo: ");
+        nombreAdmin = JOptionPane.showInputDialog("Ingrese el nombre del administrador: ");
 //        do{
 //            numSalas = Integer.parseInt(JOptionPane.showInputDialog("Ingrese numero de salas a registrar"));
 //            if(numSalas > 10)
 //                JOptionPane.showMessageDialog(null, "No pueden haber mas de 10 salas");
 //        } while(numSalas > 10);
 //        salas = new Sala[numSalas];
-	}
+    }
 
-	/* Este método se encarga de crear un objeto de tipo Sala, pedir los datos del mismo
+    /* Este método se encarga de crear un objeto de tipo Sala, pedir los datos del mismo
 		y validar que el objeto de la sala no esté repetido mediante su ID
 		@author José Manuel Quintero Rodríguez*/
-	public void ingresarSala() {
-		int nSalasRegistrar; // n, es el número de salas que el usuario quiere ingresar en el momento.
+    public void ingresarSala() {
+        int nSalasRegistrar; // n, es el número de salas que el usuario quiere ingresar en el momento.
 
-		do {
-			nSalasRegistrar = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número de salas a registrar: "));
-			if (nSalasRegistrar > 10) {
-				JOptionPane.showMessageDialog(null, "No pueden registrarse mas de 10 salas", "ERROR", JOptionPane.ERROR_MESSAGE);
-			}
-		} while (nSalasRegistrar > 10);
+        do {
+            nSalasRegistrar = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número de salas a registrar: "));
+            if (nSalasRegistrar > 10) {
+                JOptionPane.showMessageDialog(null, "No pueden registrarse mas de 10 salas", "ERROR", JOptionPane.ERROR_MESSAGE);
+            }
+        } while (nSalasRegistrar > 10);
 
-		for (int i = 0; i < salas.length; i++) {
+        for (int i = 0; i < salas.length; i++) {
 
-			if (i > nSalasRegistrar) {
-				break;
-			}
-			if (salas[i] == null) {
+            if (i >= nSalasRegistrar) {
+                break;
+            }
+            if (salas[i] == null) {
 
-				salas[i] = new Sala(); // Creamos un objeto de tipo sala con datos inicializados y lo guardamos en la posicion i
-				salas[i].pedirInfoSala(); // En el objeto que está en la posición i pedimos los datos y los guardamos para ese objeto
+                salas[i] = new Sala(); // Creamos un objeto de tipo sala con datos inicializados y lo guardamos en la posicion i
+                salas[i].pedirInfoSala(); // En el objeto que está en la posición i pedimos los datos y los guardamos para ese objeto
 
-				for (int j = 0; j < i; j++) {
+                for (int j = 0; j < i; j++) {
 
-					if (salas[j].numeroSala == salas[i].numeroSala) {
+                    if (salas[j].numeroSala == salas[i].numeroSala) {
 
-						JOptionPane.showMessageDialog(null, "Ya existe la sala con este numero", "ADVERTENCIA", JOptionPane.WARNING_MESSAGE);
-						i--; // Pasamos a la posicion anterior y volvemos a la misma para sobreescribir el nombre
-						break; // Aqui rompemos el ciclo interno que maneja el iterador j
-					}
-				}
-			} else {
-				JOptionPane.showMessageDialog(null, "Ya hay salas registradas", "ADVERTENCIA", JOptionPane.WARNING_MESSAGE);
-				break;
-			}
-		}
-	}
+                        JOptionPane.showMessageDialog(null, "Ya existe la sala con este numero", "ADVERTENCIA", JOptionPane.WARNING_MESSAGE);
+                        i--; // Pasamos a la posicion anterior y volvemos a la misma para sobreescribir el nombre
+                        break; // Aqui rompemos el ciclo interno que maneja el iterador j
+                    }
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "Ya hay salas registradas", "ADVERTENCIA", JOptionPane.WARNING_MESSAGE);
+                break;
+            }
+        }
+    }
 
-	/* Esté metodo permite mostrar el mapa de la sala especificando el número de esta
+    /* Esté metodo permite mostrar el mapa de la sala especificando el número de esta
 		@author José Manuel Quintero Rodríguez*/
 //    public void mostrarMapaSala(int id) {
 //        boolean existencia = false;
@@ -121,7 +126,7 @@ public class Complejo {
 //        }
 //    }
 
-	/* Este método se encarga de hacer la reserva en de una silla en una sala
+    /* Este método se encarga de hacer la reserva en de una silla en una sala
 		@author Jose Manuel Quintero Rodriguez*/
 //    public void reservarSilla(int id) {
 //        
@@ -167,18 +172,18 @@ public class Complejo {
 //        }
 //    }
 
-	/* Este método se encarga de calcular el valor de recaudo por ganancias en un dia
+    /* Este método se encarga de calcular el valor de recaudo por ganancias en un dia
 		@author Jose Manuel Quintero Rodriguez*/
-	public double calcularValorRecaudo() {
-		double recaudo = 0;
-		for (int i = 0; i < salas.length; i++) {
-			if (salas[i] != null) {
-				recaudo += salas[i].valorBoleta;
-			} else {
-				JOptionPane.showMessageDialog(null, "No hay salas registradas", "ERROR", JOptionPane.ERROR_MESSAGE);
-				break; // Este break sirve para que no me muestre el mensaje mas de una vez debido a que estoy dentro de un ciclo
-			}
-		}
-		return recaudo;
-	}
+    public double calcularValorRecaudo() {
+        double recaudo = 0;
+        for (int i = 0; i < salas.length; i++) {
+            if (salas[i] != null) {
+                recaudo += salas[i].valorBoleta;
+            } else {
+                JOptionPane.showMessageDialog(null, "No hay salas registradas", "ERROR", JOptionPane.ERROR_MESSAGE);
+                break; // Este break sirve para que no me muestre el mensaje mas de una vez debido a que estoy dentro de un ciclo
+            }
+        }
+        return recaudo;
+    }
 }
--- src/proyectofinal/Sala.java
+++ src/proyectofinal/Sala.java
@@ -5,7 +5,11 @@ import javax.swing.JOptionPane;
 /**
  * Fecha de creacion: 3 de mayo
  *
- * Esta clase se encarga de pedir los datos correspodientes a una sala, teniendo en cuenta que para registrar una sala tiene que haber al menos un complejo registrado, en donde al suministrar los datos correspodiente detallamos la forma en como será el mapa de respectiva sala, esta clase también se encarga de calcular el porcentaje de ocupación.
+ * Esta clase se encarga de pedir los datos correspodientes a una sala, teniendo
+ * en cuenta que para registrar una sala tiene que haber al menos un complejo
+ * registrado, en donde al suministrar los datos correspodiente detallamos la
+ * forma en como será el mapa de respectiva sala, esta clase también se encarga
+ * de calcular el porcentaje de ocupación.
  *
  * @author Jose Manuel Quintero Rodríguez.
  * @author Juan Ángel Riaño Quintero.
@@ -13,147 +17,153 @@ import javax.swing.JOptionPane;
  */
 public class Sala {
 
-	// Atributos de la clase.
-	int numeroSala, cantidadFilas, sillasPorFila;
-	double valorBoleta;
+    // Atributos de la clase.
+    int numeroSala, cantidadFilas, sillasPorFila;
+    double valorBoleta;
 
-	// Declaramos el arreglo de programaciones que serán como máximo 5.
-	Programacion programaciones[] = new Programacion[5];
+    // Declaramos el arreglo de programaciones que serán como máximo 5.
+    Programacion programaciones[] = new Programacion[5];
 
-	/**
-	 * Este es el método constructor de la clase que se encarga de inicializar los valoes de los atributos, Cabe destacar que la inicializacion tambien sirve para que al momento de crear un objeto de tipo Sala y guardarlo en un arreglo de objetos, este no se almacene con datos nulos.
-	 *
-	 * @author José Manuel Quintero Rodriguez.
-	 *
-	 */
-	public Sala() {
+    /**
+     * Este es el método constructor de la clase que se encarga de inicializar
+     * los valoes de los atributos, Cabe destacar que la inicializacion tambien
+     * sirve para que al momento de crear un objeto de tipo Sala y guardarlo en
+     * un arreglo de objetos, este no se almacene con datos nulos.
+     *
+     * @author José Manuel Quintero Rodriguez.
+     *
+     */
+    public Sala() {
 
-		numeroSala = 0;
-		cantidadFilas = 5;
-		sillasPorFila = 5;
-		valorBoleta = 0;
-	}
+        numeroSala = 0;
+        cantidadFilas = 5;
+        sillasPorFila = 5;
+        valorBoleta = 0;
+    }
 
-	/**
-	 * Este método se encarga de solicitar los datos de la sala además de definir el diseño del mapa de la sala.
-	 *
-	 * @author Jose Manuel Quintero Rodriguez.
-	 *
-	 */
-	public void pedirInfoSala() {
+    /**
+     * Este método se encarga de solicitar los datos de la sala además de
+     * definir el diseño del mapa de la sala.
+     *
+     * @author Jose Manuel Quintero Rodriguez.
+     *
+     */
+    public void pedirInfoSala() {
 
-		// El identificador de la sala se volverá autoincrementable.
-		numeroSala = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el identificador de la sala:"));
+        // El identificador de la sala se volverá autoincrementable.
+        numeroSala = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el identificador de la sala:"));
 
-		do {
-			valorBoleta = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor de la boleta: "));
+        do {
+            valorBoleta = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor de la boleta: "));
 
-			if (valorBoleta < 0) {
-				JOptionPane.showMessageDialog(null, "No se permiten valores negativos", "ERROR", JOptionPane.ERROR_MESSAGE);
-			}
-		} while (valorBoleta < 0);
-	}	
-	
-	/**
-	 * Este método, permite al usuario ingresar la cantidad de programaciones que desee.
-	 * @author Juan Ángel Riaño Quintero.
-	 * 
-	 */
-	public void ingresarProgramacion() {
-	
-		int nProgram, contIngresos = 0;  // nProgram, es el número de programaciones que el usuario quiera registrar en el momento.		
-		do {
-			nProgram = Integer.parseInt(JOptionPane.showInputDialog("¿Cuantos complejos desea ingresar?"));
-			
-			if (nProgram > programaciones.length) {	
-				JOptionPane.showMessageDialog(null, "No se pueden ingresar más complejos de los que tiene la empresa", "ERROR", JOptionPane.ERROR_MESSAGE);
-			}
-		} while (nProgram > programaciones.length);
-		
-		for (int i = 0; i < programaciones.length; i++) {
-			
-			if (contIngresos == nProgram) {
-				break;
-			}
-			
-			if (programaciones[i] == null) {
-				programaciones[i] = new Programacion(numeroSala);
-				programaciones[i].pedirInfoProgramacion();
-				contIngresos += 1;
-				JOptionPane.showMessageDialog(null, "Complejo registrado exitosamente");
-			}
-			
-			if (programaciones[i] != null && i >= programaciones.length && contIngresos == 0) {			
-				JOptionPane.showMessageDialog(null, "Ya hay " + programaciones.length + " complejos registrados", "ADVERTENCIA", JOptionPane.WARNING_MESSAGE);
-			}
-		}
-	}
+            if (valorBoleta < 0) {
+                JOptionPane.showMessageDialog(null, "No se permiten valores negativos", "ERROR", JOptionPane.ERROR_MESSAGE);
+            }
+        } while (valorBoleta < 0);
+    }
 
+    /**
+     * Este método, permite al usuario ingresar la cantidad de programaciones
+     * que desee.
+     *
+     * @author Juan Ángel Riaño Quintero.
+     *
+     */
+    public void ingresarProgramacion() {
 
-	/**
-	 * Este metodo se encarga de mostrar la programación que tiene la sala para un día.
-	 *
-	 * @author Jose Manuel Quintero Rodriguez.
-	 *
-	 */
-	public void mostrarProgramacion() {
+        int nProgram, contIngresos = 0;  // nProgram, es el número de programaciones que el usuario quiera registrar en el momento.		
+        do {
+            nProgram = Integer.parseInt(JOptionPane.showInputDialog("¿Cuantos complejos desea ingresar?"));
 
-		// Creamos un arreglo que almacenará un listado de películas en cartelera.
-		String listadoPeliculas = "";
-		boolean existencia = false;
+            if (nProgram > programaciones.length) {
+                JOptionPane.showMessageDialog(null, "No se pueden ingresar más complejos de los que tiene la empresa", "ERROR", JOptionPane.ERROR_MESSAGE);
+            }
+        } while (nProgram > programaciones.length);
 
-		// Recorremos el arreglo de programaciones.
-		for (int i = 0; i < programaciones.length; i++) {
+        for (int i = 0; i < programaciones.length; i++) {
 
-			// Verificamos que el objeto Película en la posición indicada, exista en el arreglo.
-			if (programaciones[i] != null) {
+            if (contIngresos == nProgram) {
+                break;
+            }
 
-				if (programaciones[i].peli.actividad == true) {
+            if (programaciones[i] == null) {
+                programaciones[i] = new Programacion(numeroSala);
+                programaciones[i].pedirInfoProgramacion();
+                contIngresos += 1;
+                JOptionPane.showMessageDialog(null, "Complejo registrado exitosamente");
+            }
 
-					// Indicamos si la pelicula está activa para agregarla a la programación.
-					existencia = true;
-					listadoPeliculas += (i + 1) + ". " + programaciones[i].peli.nomEspanol
-					+ "\nHorario: " + programaciones[i].horario + "\nNumero de la sala: " + numeroSala;
-				}
-			} else {
+            if (programaciones[i] != null && i >= programaciones.length && contIngresos == 0) {
+                JOptionPane.showMessageDialog(null, "Ya hay " + programaciones.length + " complejos registrados", "ADVERTENCIA", JOptionPane.WARNING_MESSAGE);
+            }
+        }
+    }
 
-				JOptionPane.showMessageDialog(null, "No se puede mostrar la programación\n"
-				+ "si no hay peliculas registradas", "ERROR", JOptionPane.ERROR_MESSAGE);
-				break;
-			}
-		}
-		// Mostramos el listado con la programación.
-		if (existencia == true) {
-			JOptionPane.showMessageDialog(null, listadoPeliculas);
-		}
-	}
+    /**
+     * Este metodo se encarga de mostrar la programación que tiene la sala para
+     * un día.
+     *
+     * @author Jose Manuel Quintero Rodriguez.
+     *
+     */
+    public void mostrarProgramacion() {
 
-	/* Este método se encarga de hacer la reserva en de una silla en una sala
+        // Creamos un arreglo que almacenará un listado de películas en cartelera.
+        String listadoPeliculas = "";
+        boolean existencia = false;
+
+        // Recorremos el arreglo de programaciones.
+        for (int i = 0; i < programaciones.length; i++) {
+
+            // Verificamos que el objeto Película en la posición indicada, exista en el arreglo.
+            if (programaciones[i] != null) {
+
+                if (programaciones[i].peli.actividad == true) {
+
+                    // Indicamos si la pelicula está activa para agregarla a la programación.
+                    existencia = true;
+                    listadoPeliculas += (i + 1) + ". " + programaciones[i].peli.nomEspanol
+                            + "\nHorario: " + programaciones[i].horario + "\nNumero de la sala: " + numeroSala;
+                }
+            } else {
+
+                JOptionPane.showMessageDialog(null, "No se puede mostrar la programación\n"
+                        + "si no hay peliculas registradas", "ERROR", JOptionPane.ERROR_MESSAGE);
+                break;
+            }
+        }
+        // Mostramos el listado con la programación.
+        if (existencia == true) {
+            JOptionPane.showMessageDialog(null, listadoPeliculas);
+        }
+    }
+
+    /* Este método se encarga de hacer la reserva en de una silla en una sala
 		@author Jose Manuel Quintero Rodriguez*/
-	public void reservarAsiento(int id) {
+    public void reservarAsiento(int id) {
 
-		boolean existencia = false;
-		int controladorAlertas = 0;
-		int confirmarReserva; // variable que indica si la confimacion de la reserva
-		int confirmador, espFila, espColumna;
-		for (int i = 0; i < programaciones.length; i++) {
-			if (programaciones[i].numeroSala == id) {
-				do {
-					espFila = Integer.parseInt(JOptionPane.showInputDialog("Dime el numero de fila"));
-					if (espFila < 0 || espFila > programaciones[i].sillas.length - 1) {
-						JOptionPane.showMessageDialog(null, "No se permite el número", "ERROR", JOptionPane.ERROR_MESSAGE);
-					}
-				} while (espFila < 0 || espFila > programaciones[i].sillas.length - 1);
-				do {
-					espColumna = Integer.parseInt(JOptionPane.showInputDialog("Dime el numero de columna"));
-					if (espColumna < 0 || espColumna > programaciones[i].sillas[0].length - 1) {
-						JOptionPane.showMessageDialog(null, "No se permite el número", "ERROR", JOptionPane.ERROR_MESSAGE);
-					}
-				} while (espColumna < 0 || espColumna > programaciones[i].sillas[0].length - 1);
-				programaciones[i].sillas[espFila][espColumna] = false;
-				break;
-			}
-		}
+        boolean existencia = false;
+        int controladorAlertas = 0;
+        int confirmarReserva; // variable que indica si la confimacion de la reserva
+        int confirmador, espFila, espColumna;
+        for (int i = 0; i < programaciones.length; i++) {
+            if (programaciones[i].numeroSala == id) {
+                do {
+                    espFila = Integer.parseInt(JOptionPane.showInputDialog("Dime el numero de fila"));
+                    if (espFila < 0 || espFila > programaciones[i].sillas.length - 1) {
+                        JOptionPane.showMessageDialog(null, "No se permite el número", "ERROR", JOptionPane.ERROR_MESSAGE);
+                    }
+                } while (espFila < 0 || espFila > programaciones[i].sillas.length - 1);
+                do {
+                    espColumna = Integer.parseInt(JOptionPane.showInputDialog("Dime el numero de columna"));
+                    if (espColumna < 0 || espColumna > programaciones[i].sillas[0].length - 1) {
+                        JOptionPane.showMessageDialog(null, "No se permite el número", "ERROR", JOptionPane.ERROR_MESSAGE);
+                    }
+                } while (espColumna < 0 || espColumna > programaciones[i].sillas[0].length - 1);
+                programaciones[i].sillas[espFila][espColumna] = false;
+                break;
+            }
+        }
 
 //        for (int i = 0; i < sillas.length; i++) { // Recorremos el arreglo de la sala
 //                if (numeroSala == id) { // Verificamos si existe la sala
@@ -186,50 +196,59 @@ public class Sala {
 //        if (existencia == false && controladorAlertas == 0) {
 //            JOptionPane.showMessageDialog(null, "No se encontró la sala", "ERROR", JOptionPane.ERROR_MESSAGE);
 //        }
-	}
+    }
 
-	public void mostrarMapaSala(int id) {
-		boolean existencia = false;
-		String muestreMapa = "";
-		for (int i = 0; i < programaciones.length; i++) { // Recorremos el arreglo de la sala
-			if (programaciones[i].numeroSala == id) { // Verificamos si la sala existe
-				for (int j = 0; j < programaciones[i].sillas.length; j++) {
-					for (int k = 0; k < programaciones[i].sillas[0].length; k++) {
-						muestreMapa += programaciones[i].sillas[j][k] + " ";
-					}
-					muestreMapa += "\n";
-				}
-				JOptionPane.showMessageDialog(null, muestreMapa);
-			} else {
-				JOptionPane.showMessageDialog(null, "No existe una sala con ese número", "ERROR", JOptionPane.ERROR_MESSAGE);
-				break; // Rompemos el ciclo inmediatamente sacandonos del ciclo
-			}
-		} // En caso de que la existencia de esa sala especifica sea falsa
-		if (existencia == true) {
-			JOptionPane.showMessageDialog(null, muestreMapa);
-		}
-	}
+    public void mostrarMapaSala(int id) {
+        boolean existencia = false;
+        String muestreMapa = "";
+        for (int i = 0; i < programaciones.length; i++) { // Recorremos el arreglo de la sala
+            if (programaciones[i] != null) {
+                if (programaciones[i].numeroSala == id) { // Verificamos si la sala existe
+                    for (int j = 0; j < programaciones[i].sillas.length; j++) {
+                        for (int k = 0; k < programaciones[i].sillas[0].length; k++) {
+                            muestreMapa += programaciones[i].sillas[j][k] + " ";
+                        }
+                        muestreMapa += "\n";
+                    }
+                    JOptionPane.showMessageDialog(null, muestreMapa);
+                } else {
+                    JOptionPane.showMessageDialog(null, "No existe una sala con ese número", "ERROR", JOptionPane.ERROR_MESSAGE);
+                    break; // Rompemos el ciclo inmediatamente sacandonos del ciclo
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "Programacion Inexistente", "ADVERTENCIA", JOptionPane.WARNING_MESSAGE);
+            }
 
-	/**
-	 * Este método se encarga de calcular el porcetaje de ocupación en una sala especifical, tomando como parametro de entrada el numero de personas que visitaron dicha sala.
-	 *
-	 * @author Jose Manuel Quintero Rodriguez.
-	 *
-	 * @param nPersonas es el parametro que nos indica cuatas reservas se hicieron para una película.
-	 *
-	 * @return El metodo debe retornar un valor Real, el cual será el porcentaje de ocupación.
-	 *
-	 */
-	public double calcularPorcOcupacion(int nPersonas) {
+        } // En caso de que la existencia de esa sala especifica sea falsa
+        if (existencia == true) {
+            JOptionPane.showMessageDialog(null, muestreMapa);
+        }
+    }
 
-		double porcentaje = 0;
+    /**
+     * Este método se encarga de calcular el porcetaje de ocupación en una sala
+     * especifical, tomando como parametro de entrada el numero de personas que
+     * visitaron dicha sala.
+     *
+     * @author Jose Manuel Quintero Rodriguez.
+     *
+     * @param nPersonas es el parametro que nos indica cuatas reservas se
+     * hicieron para una película.
+     *
+     * @return El metodo debe retornar un valor Real, el cual será el porcentaje
+     * de ocupación.
+     *
+     */
+    public double calcularPorcOcupacion(int nPersonas) {
 
-		// Verfificamos que la cantidad de personas no sea mayor al numero de sillas de la sala
-		if (nPersonas > (cantidadFilas * sillasPorFila)) {
-			JOptionPane.showMessageDialog(null, "No pueden haber mas personas que sillas en la sala", "ERROR", JOptionPane.ERROR_MESSAGE);
-		} else {
-			porcentaje = nPersonas * 100 / (cantidadFilas * sillasPorFila);
-		}
-		return porcentaje;
-	}
+        double porcentaje = 0;
+
+        // Verfificamos que la cantidad de personas no sea mayor al numero de sillas de la sala
+        if (nPersonas > (cantidadFilas * sillasPorFila)) {
+            JOptionPane.showMessageDialog(null, "No pueden haber mas personas que sillas en la sala", "ERROR", JOptionPane.ERROR_MESSAGE);
+        } else {
+            porcentaje = nPersonas * 100 / (cantidadFilas * sillasPorFila);
+        }
+        return porcentaje;
+    }
 }
